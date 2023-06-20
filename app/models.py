ca47096aff517239cb6279e9fa36a39666228c15
Archer-SN <superkakanoob@gmail.com>
2023-06-20 20:23:30 +0700

    Created a skeleton code

--- app/models.py
+++ app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class User:
+    pass
+
+# Food class has data about the amount of calories, macronutrients, and nutrients.
+class Food:
+    pass
+
+# A collection of Food
+class Recipe:
+    pass
+
+# DailyEntry contains information about your total calories intake for the day, exercised, etc.
+class DailyEntry:
+    pass
--- app/views.py
+++ app/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+
+from .models import *
+
+# This function handles the index page
+# It just renders a html file
+def index(request):
+    return HttpResponse("Hello")
+
+def login():
+    pass
+
+def register():
+    pass
+
+def diary():
+    pass
+
