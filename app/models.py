6cf84009a23ec5948d36bab2cb747364505e3baa
Archer-SN <superkakanoob@gmail.com>
2023-06-21 19:54:49 +0700

    Added unit conversion for User class

--- app/models.py
+++ app/models.py
@@ -13,14 +13,35 @@ unit_conversions = {
     ("cm", "in"): (1 / 2.54),
     ("kg", "lbs"): 2.205,
     ("lbs", "kg"): (1/2.205),
+    ("in", "ft"): (1/12),
+    ("ft", "in"): 12,
 }
+
+ACTIVITY_LEVEL = []
 
 
 # Create your models here.
 
 class User(AbstractUser):
-    weight = models.FloatField()
-    height = models.FloatField()
+    # Store weight in kg
+    weight = models.IntegerField()
+    # Store height in cm
+    height = models.IntegerField()
+    body_fat = models.FloatField()
+    bmi = models.FloatField()
+    activity_level = models.IntegerField()
+
+
+    def to_lbs(self):
+        return round(self.weight * unit_conversions[("kg", "lbs")])
+
+    # Convert the user height from cm to f
+    def to_ft_in(self):
+        # Convert height from centimeters to feet
+        height_in_ft = self.height * unit_conversions[("cm", "ft")]
+        # Convert the decimal part to inches
+        remaining_height = (height_in_ft % 10) * unit_conversions[("ft", "in")]
+        return [round(height_in_ft), round(remaining_height)]
 
 
 # Food class has data about the amount of calories, macronutrients, and nutrients.
