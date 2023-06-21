e98363267f635317fce602e83036410ff3606c64
Archer-SN <superkakanoob@gmail.com>
2023-06-21 20:39:59 +0700

    Started adding attributes to Food and DailyEntry

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -12,31 +13,24 @@ unit_conversions = {
     ("cm", "ft"): (1 / 30.48),
     ("cm", "in"): (1 / 2.54),
     ("kg", "lbs"): 2.205,
-    ("lbs", "kg"): (1/2.205),
-    ("in", "ft"): (1/12),
+    ("lbs", "kg"): (1 / 2.205),
+    ("in", "ft"): (1 / 12),
     ("ft", "in"): 12,
-    ("cm", "m"): (1/100),
+    ("cm", "m"): (1 / 100),
     ("m", "cm"): 100,
 }
+
 
 # Create your models here.
 
 class User(AbstractUser):
-    # This will be multiplied to BMR which will give TDEE
-    NONE = 1
-    SEDENTARY = 1.2
-    LIGHT = 1.35
-    MODERATE = 1.5
-    VERY = 1.9
-    ACTIVITY_LEVEL = [
-        (NONE, "None"),
-        (SEDENTARY, "Sedentary"),
-        (LIGHT, "Lightly Active"),
-        (MODERATE, "Moderately Active"),
-        (VERY, "Very Active"),
-        # TODO
-        (1, "Custom"),
-    ]
+    # This will be multiplied to BMR which will give Total Daily Energy Expenditure (TDEE)
+    class ActivityLevel(models.TextChoices):
+        NONE = 1, _("None")
+        SEDENTARY = 1.2, _("Sedentary")
+        LIGHT = 1.35, _("Lightly Active")
+        MODERATE = 1.5, _("Moderately Active")
+        VERY = 1.9, _("Very Active")
 
     SEX_CHOICES = [("M", "Male"), ("F", "Female")]
     sex = models.CharField(max_length=1, choices=SEX_CHOICES)
@@ -46,8 +40,13 @@ class User(AbstractUser):
     # Store height in cm
     height = models.IntegerField()
     body_fat = models.FloatField()
-    activity_level = models.IntegerField()
 
+    activity_level = models.FloatField(choices=ActivityLevel)
+
+    # TODO : Macronutrients target for protein, carbs, and fats.
+
+    def __str__(self):
+        return self.username
 
     def to_lbs(self):
         return round(self.weight * unit_conversions[("kg", "lbs")])
@@ -64,9 +63,15 @@ class User(AbstractUser):
     def get_bmi(self):
         return self.weight / pow((self.height * unit_conversions[("cm", "m")]), 2)
 
+    def get_calories(self):
+        # TODO
+        pass
+
+
 # Food class has data about the amount of calories, macronutrients, and nutrients.
 class Food(models.Model):
-    pass
+    name = models.CharField()
+
 
 
 # A collection of Food
@@ -76,4 +81,9 @@ class Recipe(models.Model):
 
 # DailyEntry contains information about your total calories intake for the day, exercised, etc.
 class DailyEntry(models.Model):
-    pass
+    food = models.ManyToManyField(Food, blank=True)
+    # TODO: Track Macronutrients
+    # TODO: Return the total calories consumed
+    def total_calories(self):
+        pass
+
