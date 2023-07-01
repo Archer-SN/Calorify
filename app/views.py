e5510d978973309cd2ac396444755de2e94f2c11
Archer-SN <superkakanoob@gmail.com>
2023-07-01 21:02:39 +0700

    Removed populate.py
    Former-commit-id: 4a30a08c3fd92ad16fd8c0625f726484e8622464

--- app/views.py
+++ app/views.py
@@ -1,11 +1,17 @@
+from app.scripts import credentials
 
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
 import requests
 import json
 
+
 from datetime import datetime
 from .models import *
+
+# Id and Keys for the food database api
+EDAMAM_APP_ID = "35db61c2"
+EDAMAM_KEY = credentials.EDAMAM_KEY
 
 # This function handles the index page
 # It just renders a html file
@@ -15,7 +21,7 @@ def index(request):
         user.set_password("123")
         user.save()
     daily_entry = DailyEntry.objects.create(user=user)
-    r = requests.get("https://api.edamam.com").json()
+    r = requests.get("https://api.edamam.com?app_id={app_id}&app_key={app_key}").json()
 
 
     return HttpResponse(requests)
